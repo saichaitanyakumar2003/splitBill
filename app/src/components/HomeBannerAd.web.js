de20db4c08c4debcf9ef74e53ce364bbc6535aa5
saichaitanyakumar2003 <saichaitanyakumar.chettupalli@deliveroo.com>
2026-02-20 21:59:08 +0530

    ad fix

---
+++ app/src/components/HomeBannerAd.web.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+/**
+ * Web stub: AdMob has no native component on web, so we render nothing.
+ * This file is used when building for web so we never import react-native-google-mobile-ads.
+ */
+export default function HomeBannerAd() {
+  return null;
+}
